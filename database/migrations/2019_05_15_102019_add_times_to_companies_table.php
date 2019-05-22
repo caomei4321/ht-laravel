b21ee827f827bbb25dc7952d9a52d22ca7cd828a
cm <43211374@qq.com>
2019-05-22 06:03:53 +0000

    改了个bug

--- database/migrations/2019_05_15_102019_add_times_to_companies_table.php
+++ database/migrations/2019_05_15_102019_add_times_to_companies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTimesToCompanies extends Migration
+class AddTimesToCompaniesTable extends Migration
 {
     /**
      * Run the migrations.
