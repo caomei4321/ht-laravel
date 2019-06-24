434a41c90abe45d51c221390b79e9acb661a5ced
cm <43211374@qq.com>
2019-06-24 07:53:35 +0000

    断点续传测试

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -84,6 +84,7 @@ class UsersController extends Controller
      * */
     public function breakpointRecord(Request $request)
     {
+        return $request->all();
         $personInfos = $request->only('personInfos');
 
         foreach ($personInfos['personInfos'] as $k=>$v) {
