883aea2de6799da196b7f1039877ec144527b916
cm <43211374@qq.com>
2019-07-04 02:32:57 +0000

    emm

--- app/Http/Controllers/Api/FileUploadController.php
+++ app/Http/Controllers/Api/FileUploadController.php
@@ -36,6 +36,9 @@ class FileUploadController extends Controller
         if (count($files) == $request->chunks) {
             sort($files);
 
+            if (!directoryExists(public_path() . '/uploads/android_version')) {
+                mkdir(public_path() . '/uploads/android_version', 0777,true);
+            }
             // 完整文件存储地址
 //            $fp = public_path() . '/uploads/android_version/' . $request->name;
 
